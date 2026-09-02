f74225278aa8fdb21b90a617d8bc18d43638129a
pheonix1510 <soniharshil.1510@gmail.com>
2026-09-02 21:35:29 +0530

    frontend

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const welcome = document.querySelector(".welcome");
+
+    if (welcome) {
+        welcome.classList.add("show");
+    }
+
+});
