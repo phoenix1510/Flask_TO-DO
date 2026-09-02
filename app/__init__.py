2ee93b40c6aafc130a07e1e4996c753cc7698823
pheonix1510 <soniharshil.1510@gmail.com>
2026-09-02 21:34:58 +0530

    change app factory to initialize csrf

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,11 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    from app.routes.auth import auth_bp, main_bp
+    app.config.from_object('app.config.config.Config')  # Load configuration from config.py
+    from flask_wtf.csrf import CSRFProtect
+    csrf = CSRFProtect(app)  # Enable CSRF protection
+    from app.routes.auth import auth_bp
+    from app.routes.main import main_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     return app
