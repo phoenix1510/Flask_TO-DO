ea95ca4e8eeacf458c71b9a8bb70e0a5e2ef6601
pheonix1510 <soniharshil.1510@gmail.com>
2026-09-02 22:28:09 +0530

    changes to fix and add features

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,6 @@
 # main dashboard blueprint for the application
 from turtle import title
-
+from app.routes.wtfform import addTaskForm, editTaskForm
 from flask import Blueprint, render_template, session, redirect, url_for, flash,request
 from app.db.db_operations import add_task_to_user, get_all_tasks, delete_task_from_user ,edit_task_of_user
 
@@ -18,7 +18,10 @@ def dashboard():
 
     tasks = get_all_tasks(session['user_id'])
 
-    return render_template('dashboard.html',username=session['user'],tasks=tasks)
+    add_form = addTaskForm()
+    edit_form = editTaskForm()
+
+    return render_template('dashboard.html',username=session['user'],tasks=tasks, add_form=add_form, edit_form=edit_form)
 
 @main_bp.route('/dashboard/add_task', methods=['POST'])
 def add_task():
@@ -26,8 +29,10 @@ def add_task():
         flash('Please log in to add tasks.', 'warning')
         return redirect(url_for('auth.login'))
 
-    title = request.form.get('title')
-    description = request.form.get('description')
+    form = addTaskForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
 
     if title and description:
         add_task_to_user(title, description, session['user_id'])
@@ -47,19 +52,28 @@ def delete_task(task_id):
     flash('Task deleted successfully!', 'success')
     return redirect(url_for('main.dashboard'))
 
-@main_bp.route('/dashboard/edit_task', methods=['POST'])
-def edit_task():
-    if 'user' not in session: 
+@main_bp.route('/dashboard/edit_task/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+
+    if 'user' not in session:
         flash('Please log in to edit tasks.', 'warning')
         return redirect(url_for('auth.login'))
 
-    title= request.form.get('title')
-    description = request.form.get('description')
-    task_id =request.form.get('task_id')
+    form = editTaskForm()
 
-    if task_id and title and description:
-        edit_task_of_user(task_id, title, description, session['user_id'])  
+    if form.validate_on_submit():
+
+        title = form.title.data
+        description = form.description.data
+
+        edit_task_of_user(
+            task_id,
+            title,
+            description,
+            session['user_id']
+        )
+
         flash('Task updated successfully!', 'success')
-    else:
-        flash('Task title and description cannot be empty.', 'danger')
-    return redirect(url_for('main.dashboard'))
+        return redirect(url_for('main.dashboard'))
+
+    return render_template('edit.html', edit_form=form,task_id=task_id)
--- app/routes/wtfform.py
+++ app/routes/wtfform.py
@@ -1,10 +1,19 @@
 # initialize wtf form class
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=25)])
     submit = SubmitField('Login')
-    
+
+class addTaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Add Task')
+
+class editTaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Edit Task')
